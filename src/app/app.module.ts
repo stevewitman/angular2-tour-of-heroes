4ea109f359cf421580b64bee8eb557fd8b6086b5
Steve Witman <stevewitman@gmail.com>
2017-03-16 23:01:43 -0600

    Introduce router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+
+
 
 @NgModule({
   declarations: [
@@ -17,35 +20,15 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-// import { NgModule }       from '@angular/core';
-// import { BrowserModule }  from '@angular/platform-browser';
-// import { FormsModule }    from '@angular/forms';
-// import { AppComponent }        from './app.component';
-// import { HeroDetailComponent } from './hero-detail.component';
-// import { HeroesComponent }     from './heroes.component';
-// import { HeroService }         from './hero.service';
-// @NgModule({
-//   imports: [
-//     BrowserModule,
-//     FormsModule
-//   ],
-//   declarations: [
-//     AppComponent,
-//     HeroDetailComponent,
-//     HeroesComponent
-//   ],
-//   providers: [
-//     HeroService
-//   ],
-//   bootstrap: [ AppComponent ]
-// })
-// export class AppModule {
-// }
